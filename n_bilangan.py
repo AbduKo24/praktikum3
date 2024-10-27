e74d948f68b2c14fe9c84f6cc2ab7f1630b64c17
AbduKo24 <muhamadabdu2005@gmail.com>
2024-10-27 16:09:25 +0700

    Add files via upload

---
+++ n_bilangan.py
@@ -0,0 +1,6 @@
+max = 0
+bilangan = int(input("masukan bilangan :"))
+while bilangan != 0 :
+    if bilangan > max :
+        bilangan = int(input("masukan bilangan:"))
+print (f"bilangan terbesar= {max}")
